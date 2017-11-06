8e4101de4c8d1b434bd7415a17da4d7ddaa8b574
Alex Colpitts <alex.colpitts@gmail.com>
2017-11-06 11:45:53 -0400

    Adding in Display
    Completed simulation, parameters can now be changed to see final results.

--- Communication/CommunicationLink.m
+++ Communication/CommunicationLink.m
@@ -7,18 +7,21 @@ clc
 
 figure_count = 1;
 %% system parameters
-BW = 600E3; % approximate bandwidth, Hz
+BW = 2E6; % approximate bandwidth, Hz, BW = 2*(max_data_rate), ideally 1 Mbps
 r = 400E3; % meters, altitude
-f = [146E6 440E6 915E6 2450E6]; % f_c, amateur, amateur and ISM
+f = [146E6 440E6 915E6 2450E6]; % f_c, amateur, amateur ISM ISM 
 lambda = 3E8./f;
 
 den = 1.2922; % density of air at 273 K 
 P = 101300.0; % pressure in pascal
-T = 273; % temperature in kelvin
+T = 300; % temperature in kelvin, assume pretty warm
 
 % antennas gains
 G_tx = 1.5; % dB, gain from dipole antenna on CubeSat
 G_rx = 15; % dB, gain from cross-polarized yagi for ground station
+
+% chip currently with best weighted means score...
+P_tx = 13; % dBm
 
 % contants
 r_earth = 6371E3; %m
@@ -77,5 +80,43 @@ p4 = -0.11293;
 % function must be handed the frequency as f/1e9
 attn_gas_dB = @(x) (p1*x^3 + p2*x^2 + p3*x + p4);
 
-attn_915 = attn_gas_dB(915E6/1E9) % 2.33 dB attenuation at 915 MHz
-% very small attenuation change between 
+attn_915 = attn_gas_dB(915E6/1E9); % 2.33 dB attenuation at 915 MHz
+% very small attenuation change between having water vapor density at 0
+% (dry air) and 15 (lots of moisture)
+
+%% Total attenuation and Antenna Gains
+% atmospheric gas losses and free space path losses
+total_attn = attn_915 + [-max(path_loss(:,3)), -min(path_loss(:,3))]; 
+
+% transmit dipole on cubesat, cross polarized yagi on earth 
+total_gain = G_rx + G_tx;
+
+link_total = total_gain - total_attn; % best and worse case attenuation
+
+above_NF = link_total - NF_dB + P_tx; % amount rx signal is above noise floor
+
+disp(sprintf('Best and Worse Case Scenarios'))
+disp(sprintf('Total Attenuation:    %0.2f (dB)\n', total_attn))
+disp(sprintf('\nTotal Gain:           %0.2f (dB)', total_gain))
+disp(sprintf('Transmit Power:       %0.2f (dBm)\n\n', P_tx))
+disp(sprintf('Best and Worse Case Scenarios'))
+disp(sprintf('Above Noise Floor:    %0.2f (dB)\n', above_NF))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
