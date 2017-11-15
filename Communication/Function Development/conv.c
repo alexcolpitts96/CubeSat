236bfb0998600898d7eec09ce919e95b77de676d
Alex Colpitts <alex.colpitts@gmail.com>
2017-11-15 19:10:48 -0400

    Auto shifting added
    Data array will now be autoshifted until a 1 is the MSB

--- Communication/Function Development/conv.c
+++ Communication/Function Development/conv.c
@@ -8,18 +8,18 @@
 #include "stdlib.h"
 #include "inttypes.h"
 
-// use Barker Code length 5 where 1 = 1 and -1 = 0 00000
+// use Barker Code length 5 where 1 = 1 and -1 = 0 00000 11101
 #define PN 0b11101
 #define PN_LENGTH 5
+#define TX_LENGTH_MASK 0x8000000000000000 // mask to check if data has been shifted all the way over
 
 //#define PN 0b110
 //#define PN_LENGTH 3
 
 // convolve a PN sequences against data, returns 
-int64_t data_conv(uint8_t data){
+uint64_t data_conv(uint8_t data){
 	uint8_t i;
-	int64_t tx_data = 0;
-	int shift;
+	uint64_t tx_data = 0;
 			
 	for(i = 0; i < sizeof(data)*8; i++){ // number of bytes * bits/byte = number of bits in data
 				
@@ -31,17 +31,21 @@ int64_t data_conv(uint8_t data){
 	}
 	
 	// shift all the way to end to allow error correcting bits to be added at the end
-	shift = sizeof(tx_data)*8 - PN_LENGTH*sizeof(data)*8;
-	printf("Shift: %d\n", shift);
 	
-	return tx_data;
+	while(!(tx_data & TX_LENGTH_MASK)){ // shift until 1 is the MSB
+		tx_data = tx_data << 1;
+	}
+	
+	return tx_data; // return the data block
 };
 
 int main(){
 	uint8_t data = 0b11110000; // assume camera takes 8 bit pixels x3 for RGB
-	int64_t temp;
+	uint64_t temp;
 		
 	temp = data_conv(data);
+	
+	printf("%" PRIu64 "\n", temp);
 
 	return 0;
 }
