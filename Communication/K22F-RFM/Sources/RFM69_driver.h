01364e20b35234f1a94d51df29b66dd449a77e87
Alex Colpitts <alex.colpitts@gmail.com>
2018-01-31 09:15:02 -0400

    RFM config data structure created

--- Communication/K22F-RFM/Sources/RFM69_driver.h
+++ Communication/K22F-RFM/Sources/RFM69_driver.h
@@ -6,42 +6,50 @@
 #define RFM_WRITE 0x80
 #define RFM_READ 0x00
 
+// configuration structure has been modified from https://github.com/LowPowerLab/RFM69/blob/master/RFM69.cpp
+const uint8_t CONFIG[][2] =
+{
+		// standbye with sequencer during config
+	{REG_OPMODE, RF_OPMODE_SEQUENCER_ON | RF_OPMODE_LISTEN_OFF | RF_OPMODE_STANDBY},
+
+	// continuous mode, everything else default
+	{REG_DATAMODUL, RF_DATAMODUL_DATAMODE_CONTINUOUS | RF_DATAMODUL_MODULATIONTYPE_FSK | RF_DATAMODUL_MODULATIONSHAPING_00},
+
+	// default speed of 4800 bps (testing purposes only)
+	{REG_BITRATEMSB, RF_BITRATEMSB_4800},
+	{REG_BITRATELSB, RF_BITRATELSB_4800},
+
+	// default fdev of 5kHz
+	{REG_FDEVMSB, RF_FDEVMSB_5000},
+	{REG_FDEVLSB, RF_FDEVLSB_5000},
+
+	// set carrier to 915 MHz (this is the default value)
+	{REG_FRFMSB, RF_FRFMSB_915},
+	{REG_FRFMID, RF_FRFMID_915},
+	{REG_FRFLSB, RF_FRFLSB_915},
+
+	// power level setting -18 dBm +  18 dBm (0b10010) = 0 dBm
+	{REG_PALEVEL, RF_PALEVEL_PA0_ON | RF_PALEVEL_PA1_OFF | RF_PALEVEL_PA2_OFF | RF_PALEVEL_OUTPUTPOWER_10010},
+
+	// over current protection, max draw of 95 mA
+	{REG_OCP, RF_OCP_ON | RF_OCP_TRIM_95},
+
+	// set to recommended default, reset values are different
+	{REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_24 | RF_RXBW_EXP_5},
+
+	// ignore DIO registers for now
+
+	// RSSI Threshold setting
+	{REG_RSSITHRESH, RF_RSSITHRESH_VALUE}, // -value/2 (dBm)
+
+	// give system stopping point for loops when CONFIG[i][0] == 255...
+	{255, 0}
+};
+
 void RFM69_Init(){
-	uint8_t temp;
-	SPI0_Prep(); // prepare SPI
 
-	// see page 60 in RFM user manual for register details
 
-	// module set to standby by default, ok for configuration
 
-	// set to continuous mode, fsk (default)
-	SPI0_TX((RFM_WRITE | REG_DATAMODUL) << 8 | RF_DATAMODUL_DATAMODE_CONTINUOUS);
-
-	// set bitrate of the communication link to 300 kbps
-	//SPI0_TX((RFM_WRITE | REG_BITRATEMSB) << 8 | RF_BITRATEMSB_300000);
-	//SPI0_TX((RFM_WRITE | REG_BITRATELSB) << 8 | RF_BITRATELSB_300000);
-
-	// must follow: frequency deviation + bitrate/2 <= 500kHz, f_dev = 200kHz
-	//SPI0_TX((RFM_WRITE | REG_FDEVMSB) << 8 | RF_FDEVMSB_200000);
-	//SPI0_TX((RFM_WRITE | REG_FDEVLSB) << 8 | RF_FDEVLSB_200000);
-
-	// leave REG_FRFxxx to default values (915 MHz)
-
-	// Calibrate RC Oscillator
-	SPI0_TX((RFM_WRITE | REG_OSC1) << 8 | RF_OSC1_RCCAL_START);
-	temp = 0;
-
-	while(!temp){
-		SPI0_TX((RFM_READ | REG_OSC1) << 8); // make read request
-		temp = SPI0_RX() & RF_OSC1_RCCAL_DONE; // mask all bits except for RCCAL_DONE
-	}
-
-	// leave RegAfcCtrl as default
-
-	// ignore RegListenX for now
-
-	// set power level to 0 (dBm)
-	SPI0_TX(((RFM_WRITE | REG_PALEVEL) << 8) | RF_PALEVEL_PA0_ON | RF_PALEVEL_PA1_OFF | RF_PALEVEL_PA2_OFF | RF_PALEVEL_OUTPUTPOWER_10010);
 }
 
 void RFM69_TX_Prep(){
