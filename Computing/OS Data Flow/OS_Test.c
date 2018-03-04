1de5f3b6104141440ed4e95aed79acd20c7a5050
Hsuan-Wei Lo <32223051+hsuanweilo@users.noreply.github.com>
2018-03-04 12:15:33 -0400

    Updated OS

--- Computing/OS Data Flow/OS_Test.c
+++ Computing/OS Data Flow/OS_Test.c
@@ -9,23 +9,27 @@ Project: OS data flow Rev 1.0
 Author: Hsuan-Wei Lo 3476309
 Created Feb 14, 2018
 
-Updated Feb 28,2018
+Updated March 4,2018
 Sleep Flag Fixed rev. 2
 Continue on ADC reading on the battery/GPIO on and off switch for camera and transmitter
+GPIO LED indication for low battery level and solar power
 /+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++/
 log:
 Feb 28 Combine ADC reading with check bat into one function
 /+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++/
 */
 
+//------------Define_Fuction--------------//
 void init_sys_module();
 void sleep_now();
 void check_bat();
+void deinit_sys_module();
+void LED();
 
-
-void ADC0_Init ();
+void  ADC0_Init ();
 float ADC0_Convert();
 
+//----------------------------------------//
 //Global Flag Register
 int check_bat_flag  = 0;// State 1
 int image_iden_flag = 0;// State 2
@@ -35,31 +39,30 @@ int listen_flag = 0;  	// State 5
 int tx_flag = 0;     	// State 6
 
 //Global Sleep Flag Condition
-int sleep_flag=0;//Check battery state
+int sleep_flag = 0;//Check battery state
 
 
 
 
 int main(){
 
-
+//----------------------Global_Variable-------------//
 int reset=0;
 int request=0;
 
-int count2=0;
-int count3=0;
 
 ADC0_Init();
 
 check_bat_flag=1;//Start the first state in the while 1 loop
-
-//-------------------------------------------------
+LED();
+//--------------------------------------------------//
 		//Check battery state 1
 
 while(check_bat_flag==1){
          	//1. Put the System into sleep
         	//2. Set a delay block function
         	//3. Wakes up after the duration
+
 
         check_bat();
         if (!sleep_flag){
@@ -74,6 +77,8 @@ init_sys_module();
 //----------------------------------------------------
 //Sleep Mode Condition
 	while(sleep_flag==1){
+		LED();
+		//GPIOA_PSOR = 0x01 << 1;//PTA1 Red LED
 		//1. Put the System into sleep
 		//2. Set a delay block function
 		//3. Wakes up after the duration
@@ -124,7 +129,7 @@ init_sys_module();
 
 
 //--------------------------------------------------
-        //Transmit Preparation state 4
+        //Transmit Preparation state 4 (NO PN sequence which this state is NO GO)
 
 	while(prep_Tx_flag==1&&!sleep_flag){
         //bat=1;
@@ -160,7 +165,7 @@ init_sys_module();
 //--------------------------------------------------
         //Transmit state 6
 	while(tx_flag==1&&!sleep_flag){
-        request=0;//requestion from ground station
+        request=0;//request from ground station
         //bat=1;
 		 /*
 		//Packet request check condition
@@ -171,16 +176,17 @@ init_sys_module();
 
 		//Transmit mode:
 			//While the ground station still heard, then transmit the image
-			//If (packet_request==1){
-				//Transmit the image to the ground station function
-				//Clear the image
 
+			//If (packet_request==1){
+        	//imageSize();  //Transmit the image to the ground station function
+			//Clear the image
+              image_iden_flag=1;
 			// }
 
 			//else {listen_flag=1;tx_flag=reset;}// go back to the listen mode again if ground station lost communication
 
 
-        	image_iden_flag=1;
+
         	check_bat();
         	tx_flag=reset;//Start again from the beginning
 
@@ -191,20 +197,34 @@ init_sys_module();
 
 	}//Main Function
 
+void deinit_sys_module(){
+
+	//--------------------------SIM_MASK----------------------------//
+	//SIM_SCGC5= reset;
+
+
+
+}
 
 
 void init_sys_module(){
-
-	SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK; // Enable port A and B
+    //--------------------------SIM_MASK----------------------------//
+	//SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
 	//SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
-
-	PORTC_PCR1 |= PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK; // Use PTC1 as GPIO
-	PORTC_PCR2 |= PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK; // Use PTC2 as GPIO
+	SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK; // Enable port C
 
 
-	GPIOC_PDDR = 0x01 << 1;
-
+	//--------------------------PORT_MASK---------------------------//
+	PORTC_PCR1 |= PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK; // Use PTC1 as GPIO for camerma  switch
+	PORTC_PCR2 |= PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK; // Use PTC2 as GPIO for RFM69 switch
+	//PORTD_PCR5 |= PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK;//PTD5 Blue LED
+	//PORTA_PCR1 |= PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK;//PTA1 Red LED
+	//-------------------------GPIO_MAP-----------------------------//
+	GPIOC_PDDR = 0x01 << 1; //PTC1
+	//GPIOD_PDDR = 0x01 << 5; //PTD5 Blue LED
+	//GPIOA_PDDR = 0x01 << 1; //PTA1 Red LED
 }
+
 
 
 void ADC0_Init (){
@@ -213,7 +233,7 @@ void ADC0_Init (){
 
 	SIM_SCGC6 |= SIM_SCGC6_ADC0_MASK;//0x08000000
 
-	ADC0_CFG1 |= 0b00101000;// Set the Mode in 10 as it to have 10-bit conversion
+	ADC0_CFG1 |= 0b00111100;// Set the Mode in 16 as it to have 10-bit conversion, with long sample time
 							// Set the ADIV (clock Divide Select) ratio is 1
 	ADC0_CFG2 |= 0x00;
 	ADC0_SC2 |= 0x00;// ADTRG software triiger, DMA disabled, Voltage Refereence (REFSEL) default voltage reference =1.207
@@ -236,23 +256,43 @@ float ADC0_Convert() {
 	}
 
 
+void LED(){
+	//PTA1 Red LED
+	//PTD5 Blue LED
 
+			SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK;
+			PORTC_PCR1 |= PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK;
+
+			GPIOC_PSOR = 0x01 << 1;//turn on Camera via MOSFET switch
+
+
+			//GPIOD_PDDR = 0x01 << 5;
+			//GPIOD_PSOR = 0x01 << 5;
+			//GPIOD_PSOR = 0x01 << 5;
+
+
+}
 
 void check_bat(){
 	float batraw=0;
 	float bat=0;
 
-	//Set the All SIM Moudule to default or turn off expect ADC module
+	//Set the All SIM Module to default or turn off expect ADC module
 
 	//-------------------------ADC_Convert----------------------
 	batraw=ADC0_Convert();//Print the Read ADC value in the num
-	bat=(batraw*3.3f)/(1024.0f);
+	bat=(batraw*3.3f)/(65536.0f);
 	//----------------------------------------------------------
 	//Check the Battery level without the load.
-	if (bat<3)
+	if (bat<2.5)
 		sleep_flag=1;
 	else
-		sleep_flag=0;
+		{sleep_flag=0;
+		GPIOC_PCOR=0x01 << 1;
+		//GPIOD_PSOR = 0x01 << 5;
+		//GPIOD_PCOR = 0x01 << 5;
+		}
+
 	//Reinitialized All the SIM module clock back
 }
 
