0c0ebb1e6fe148d71bc0170c690dd8db6aed3db9
Quinn <qs1996@gmail.com>
2018-03-21 13:30:23 -0300

    camera res fix

--- Mission/k22_testbench/SPI/spi.h
+++ Mission/k22_testbench/SPI/spi.h
@@ -11,7 +11,7 @@
 void SPI1_Init(int frame_size);
 void SPI1_txPrep();
 uint16_t SPI1_RX();
-void SPI1_TX(uint16_t tx_data, int cs);
+void SPI1_TX(uint16_t tx_data);
 uint8_t SPI1_read(uint8_t regaddr);
 
 #endif /* SPI_SPI_H_ */
